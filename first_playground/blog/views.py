2c1ff8413dbe514f67d4a2dd19f3a216ba8b79ac
Jake Jackson <jljacks93@gmail.com>
2020-05-01 15:36:45 -0400

    started adding some templates

--- first_playground/blog/views.py
+++ first_playground/blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 def about_page(request):
-    return HttpResponse('<h1>About this Blog</h1>')
+    return render(request, 'blog/about.html')
+    #you got to minute 8;45 in the video
